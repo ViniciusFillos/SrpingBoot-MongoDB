edc55e59625c7ee9b63ff9fcb9bb5136cab2420e
Vinicius Fillos <vinifillos@gmail.com>
2024-04-21 09:11:57 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/vinifillos/workshopmongo/config/Instantiation.java
@@ -0,0 +1,26 @@
+package com.vinifillos.workshopmongo.config;
+
+import com.vinifillos.workshopmongo.domain.User;
+import com.vinifillos.workshopmongo.repository.UserRespository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRespository userRespository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRespository.deleteAll();
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+        userRespository.saveAll(Arrays.asList(maria,alex,bob));
+    }
+}
