0dfdb3a1d2f49ceb1e368c7ed7ce449c585851a3
Vinicius Fillos <vinifillos@gmail.com>
2024-04-21 11:55:54 -0300

    Obtendo um usuário por id

--- src/main/java/com/vinifillos/workshopmongo/services/UserService.java
+++ src/main/java/com/vinifillos/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.vinifillos.workshopmongo.services;
 
 import com.vinifillos.workshopmongo.domain.User;
 import com.vinifillos.workshopmongo.repository.UserRespository;
+import com.vinifillos.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,11 @@ public class UserService {
 
     public List<User> findAll() {
         return userRespository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> user = userRespository.findById(id);
+        if(user.isPresent()) return user.get();
+        else throw new ObjectNotFoundException("Objeto não encontrado");
     }
 }
