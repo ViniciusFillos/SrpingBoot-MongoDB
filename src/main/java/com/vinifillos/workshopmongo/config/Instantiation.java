9f52776a149ac0e939e9dea0742e8d087fb371f8
Vinicius Fillos <vinifillos@gmail.com>
2024-04-22 02:22:54 -0300

    Referenciando os posts do usuário

--- src/main/java/com/vinifillos/workshopmongo/config/Instantiation.java
+++ src/main/java/com/vinifillos/workshopmongo/config/Instantiation.java
@@ -31,12 +31,15 @@ public class Instantiation implements CommandLineRunner {
         userRespository.deleteAll();
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
-        User bob = new User(null, "Bob Grey", "bob@gmail.com"); 
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
         userRespository.saveAll(Arrays.asList(maria,alex,bob));
 
         postRespository.deleteAll();
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
         postRespository.saveAll(Arrays.asList(post1,post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRespository.save(maria); 
     }
 }
